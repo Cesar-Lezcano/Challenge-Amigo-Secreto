364cff34e903e10d28c165429b953a7b6213e5d8
Cesar Lezcano <cesarion3432269@gmail.com>
2025-08-15 21:27:40 -0300

    Actualizo lógica del Amigo Secreto

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,51 +1,52 @@
 let amigos = [];
 
-function agregarAmigo() {
-    
-    const input = document.getElementById("amigo");
-    const nombre = input.value.trim();
-
-
-    if (nombre === "") {
-        alert("Por favor, inserte un nombre.");
-        return;
-    }
-
-    amigos.push(nombre);
-
-    input.value = "";
-
-    console.log(amigos); // Para verificar que se va guardando correctamente
-
-    mostrarLista();
-
-}
-
-
-function mostrarLista() {
-    
-    const lista = document.getElementById("listaAmigos");
-
-    lista.innerHTML = "";
-
-    for (let i = 0; i < amigos.length; i++) {
-        const li = document.createElement("li"); 
-        li.textContent = amigos[i]; 
-        lista.appendChild(li);
-    }
-
-    // Función para sortear un amigo secreto
-    function sortearAmigo() {
+    function agregarAmigo() {
         
+        const input = document.getElementById("amigo");
+        const nombre = input.value.trim();
+
+
+        if (nombre === "") {
+            alert("Por favor, inserte un nombre.");
+            return;
+        }
+
+        amigos.push(nombre);
+
+        input.value = "";
+
+        console.log(amigos); // Para verificar que se va guardando correctamente
+
+        mostrarLista();
+
+    }
+
+
+    function mostrarLista() {
+        
+        const lista = document.getElementById("listaAmigos");
+
+        lista.innerHTML = "";
+
+        for (let i = 0; i < amigos.length; i++) {
+            const li = document.createElement("li"); 
+            li.textContent = amigos[i]; 
+            lista.appendChild(li);
+        }
+
+    }
+
+
+    function sortearAmigo() {
+
         if (amigos.length === 0) {
             alert("No hay amigos en la lista para sortear.");
             return;
         }
-
+        
         const indiceAleatorio = Math.floor(Math.random() * amigos.length);
         const amigoSecreto = amigos[indiceAleatorio];
-
         const resultado = document.getElementById("resultado");
-        resultado.innerHTML = ` El amigo secreto es: ${amigoSecreto}`;
+        resultado.innerHTML = `El amigo secreto es: ${amigoSecreto}`;
+
     }
-}
